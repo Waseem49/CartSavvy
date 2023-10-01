3d4f8ee36b7c4245e7c830391e312e994c227767
Md Waseem Akram <mdwaseem0603@gmail.com>
2023-10-02 02:08:54 +0530

    done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Mycontext } from "../context/ProductContext";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const { state } = useContext(Mycontext);
+  const totalPrice = state.cart.reduce((total, item) => total + item.price, 0);
+
+  return (
+    <div className="cart">
+      <h1>My Cart Items</h1>
+      <div>
+        <div>
+          <div>
+            {state.cart.length === 0 ? (
+              <p>Your cart is empty.</p>
+            ) : (
+              <div>
+                {state.cart.map((el) => (
+                  <div className="cartitem">
+                    <img src={el.thumbnail} alt="alt" />
+                    <div>
+                      <h3>{el.title.substring(0, 18)}</h3>
+                      <div>
+                        <h4>Price: ${el.price}</h4>
+                        <button>Remove</button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+        <div>
+          <div>
+            {state.cart.length > 0 && (
+              <div>
+                <h3>Order Summary</h3>
+                <p>Total: ${totalPrice.toFixed(2)}</p>
+                <button>Proceed to Checkout</button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
